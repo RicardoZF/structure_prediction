a563e2c14ebf5dea88f7d9c070cf10eb30c0f8e5
FanZhou <35528496+RicardoZF@users.noreply.github.com>
2026-01-08 18:06:30 +0800

    Reorder conda installation commands in install.sh

--- alphafold3/install.sh
+++ alphafold3/install.sh
@@ -3,8 +3,8 @@
 
 #conda create -n af3 python=3.11
 #conda activate af3
-conda install -y -c bioconda hmmer==3.3.2 hhsuite==3.3.0 kalign2==2.04
 conda install -c conda-forge libgcc libgcc-ng  -y
+conda install -y -c bioconda hmmer==3.3.2 hhsuite==3.3.0 kalign2==2.04
 pip3 install -r zf_requirements.txt
 
 pip3 install --no-deps .
