153283b483fc2220ac343e4675e7222440c9a78a
FanZhou <35528496+RicardoZF@users.noreply.github.com>
2026-04-03 18:30:00 +0800

    Modify install.sh to install biopython and update requirements
    Updated installation script to include biopython and changed requirements file.

--- alphafold3/install.sh
+++ alphafold3/install.sh
@@ -1,10 +1,11 @@
 #git clone https://github.com/google-deepmind/alphafold3.git
 #cd ./alphafold3
 
-#conda create -n af3 python=3.11
+#conda create -n af3 python=3.11 
 #conda activate af3
 conda install -c conda-forge libgcc libgcc-ng  -y
 conda install -y -c bioconda -c conda-forge hmmer==3.3.2 hhsuite==3.3.0 kalign2==2.04
-pip3 install -r zf_requirements.txt
-
+pip3 install biopython
+pip3 install -r requirements.txt
 pip3 install --no-deps .
+build_data
